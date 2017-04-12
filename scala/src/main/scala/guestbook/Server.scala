58d57579631469a4148c0b2f1d95041e8342267c
Andrés López <andres.lopez@global.11paths.com>
2017-04-12 23:22:52 +0200

    Add server. Fix project directories.

---
+++ scala/src/main/scala/guestbook/Server.scala
@@ -0,0 +1,67 @@
+package guestbook
+
+import scalaz.concurrent.Task
+
+import org.http4s.server.blaze._
+import org.http4s.server.syntax._
+import org.http4s.server.{Server, ServerApp}
+
+import org.http4s._
+//import org.http4s.circe._
+import org.http4s.dsl._
+
+/*import io.circe._
+import io.circe.generic.auto._
+import io.circe.syntax._*/
+
+import scala.concurrent.duration._
+import scala.io.Source
+
+
+// http://http4s.org/v0.15/service/
+object Boot extends ServerApp {
+
+  override def server(args: List[String]): Task[Server] = {
+
+    import GuestBookService._
+
+    var host: String = "localhost"
+    var port: Int = 8080
+
+    if (args.length > 3) {
+      System.err.println(s"Usage: ${args(0)} [host [port]]")
+      System.exit(0)
+    }
+    
+    if (args.length == 2) host = args(1)
+    else if (args.length == 3) {
+      host = args(1)
+      port = args(2).toInt
+    }
+
+    BlazeBuilder
+      .bindHttp(port, host)
+      .mountService(guestbookService, "/")
+      .start
+
+  }
+}
+
+
+object GuestBookService {
+
+  private object FromIDParam extends QueryParamDecoderMatcher[Int]("from")
+
+  val guestbookService = HttpService {
+
+    case GET -> Root / "new" :? FromIDParam(id) =>
+      Ok(s"From element: ${id}")
+
+    case GET -> Root / "new" =>
+      Ok("New elements")
+
+    case GET -> Root =>
+      Ok("Root")
+
+  }
+}
